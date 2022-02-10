5515889e449f85d44f2dafbb257dd7ba5ce07261
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-09 22:34:49 -0600

    push

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // require your server and launch it
-const express = require('express');
+const server = require('./api/server');
+
+const port = 9000;
+
+server.listen(port, () => {
+    console.log(`\n*** Listening on port ${port} ***\n`)
+});
